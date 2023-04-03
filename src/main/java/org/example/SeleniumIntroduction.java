c0646f190d4821fd099d41760875e8c168dc426c
Michael Drougas <sehser5@hotmail.com>
2023-04-03 14:26:22 -0400

    Continued implementing Automated Testing of XTW Legacy

--- src/main/java/org/example/SeleniumIntroduction.java
+++ src/main/java/org/example/SeleniumIntroduction.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,10 +8,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.time.Duration;
 
 public class SeleniumIntroduction {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
-        BasicConfigurator.configure();//required for log4j to work
+        //BasicConfigurator.configure();//required for log4j to work
 
         //invoke browser (Firefox)
         System.setProperty("webdriver.gecko.driver" ,
@@ -44,15 +44,30 @@ public class SeleniumIntroduction {
         System.out.println("\n\n\n\nHELLO");
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2)); //wait two seconds before running next step
+        //Will fail if error message not found
         if (!(driver.findElement(By.xpath("/html/body/div[1]/div[2]/form[2]/div[1]")).isDisplayed()))
         {
             driver.quit();
         }
-
         driver.findElement(By.id("Password")).sendKeys("medeco#1");
+
+        //Main menu & select "Users" tab
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/form[2]/div/fieldset/p[1]/input")).click();
-        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/ul/li[5]/a")).click();
-        driver.quit();
+        driver.findElement(By.cssSelector("li.users > a:nth-child(1)")).click();
+
+        //create new user
+        driver.findElement(By.cssSelector("div.inline-left:nth-child(3) > div:nth-child(1) > div:nth-child(1) > " +
+                "p:nth-child(1) > a:nth-child(1)")).click();
+        driver.findElement(By.cssSelector("#FirstName")).sendKeys("AAASelenium");
+        driver.findElement(By.cssSelector("#Context_LastName")).sendKeys("Test");
+        driver.findElement(By.cssSelector("#Context_UserId")).sendKeys("AAATestUser");
+        driver.findElement(By.cssSelector("#Context_Title")).sendKeys("SeleniumTest");
+        driver.findElement(By.cssSelector("#Context_WorkPhone")).sendKeys("555-555-5555");
+        driver.findElement(By.cssSelector("#Context_MobilePhone")).sendKeys("111-111-1111");
+        driver.findElement(By.id("Context_EmailAddress")).sendKeys("SeleniumTestCase@gmail.com");
+        //This is where the timezone starts...Will need to cycle through these
+        driver.findElement(By.cssSelector("#Timezone > option:nth-child(1)")).click();
+        //driver.findElement(By.cssSelector("#Timezone > option:nth-child(2)")).click();
 
 
 
